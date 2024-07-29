8aca4cdcea58ec5cf6073c62ee18d9391d6c4730
HannanAbid7 <mhannanabid@gmail.com>
2024-07-29 05:56:00 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import io from 'socket.io-client';
 import Chat from './chat.js'
 
-const socket=io.connect("http://localhost:3001");
+const socket=io.connect("https://chat-application-backend-5hkc.onrender.com");
 
 function App() {
   const [username,setUsername]=useState("");
